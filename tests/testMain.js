b7fb473fe66c0b5df19f5043d0f50dad33088383
Kim Jones <kjones15@ncsu.edu>
2022-03-10 16:16:40 -0500

    Added 5 main tests and outline for mocking tests

--- tests/testMain.js
+++ tests/testMain.js
@@ -6,6 +6,7 @@ const nock = require("nock");
 process.env.NODE_ENV = 'test'
 const bot = require("../index.js");
 const proc = require("../processing.js");
+const scrap = require("../scraping.js");
 
 // Load mock data
 const data = require("../mock.json")
@@ -18,7 +19,7 @@ const data = require("../mock.json")
 // based off of code given in HW2 and Mocking lab
 ///////////////////////////
 
-describe('Tests of other index.js:', function () {
+describe('Tests of index.js:', function () {
     // MOCK SERVICE
     // var mockService = nock("https://api.github.com")
     // // .log(console.log)
@@ -43,7 +44,9 @@ describe('Tests of other index.js:', function () {
         console.log("ReturnValue test 1: ", returnValue);
         assert(returnValue === false);
     });
+});
 
+describe('Tests of processing.js:', function () {
     //Will have to be updated when validateUserInput() is complete, but this is a start for when it is
     //Test to show an invalid message will return invalid message response
     // it("ensures that validateUserInput() returns false", function() {
@@ -72,6 +75,58 @@ describe('Tests of other index.js:', function () {
         console.log("ReturnValue test 5: ", returnValue);
         assert(returnValue === null);
     });
+
+    it("ensures that processString() returns null when there are no verbs in the message", function() {
+        // CREATE TEST OBJECT
+        // console.log("TEST: ", bot)
+        // msg = {"data": {"sender_name": "GitEx"}};
+        let returnValue = proc.processString("smurf", null);
+        console.log("ReturnValue test 6: ", returnValue);
+        assert(returnValue === null);
+    });
+
+});
+
+describe('Tests of scraping.js:', function () {
+    //Test for getting the shell command for create repo
+    const testOfShell = nock("https://api.github.com")
+      .get("/repo/create/shell")
+      .reply(200, JSON.stringify(data.repo.create.shell));
+
+    // it('Return an example of shell command for create repo', async function() {
+    //     let returnValue = await scrap.retrieveShellExample("create repo shell");
+    //     expect(returnValue).to.equal("");
+    // }); 
+
+    //Test for getting response body for create repo
+    const testOfResponse = nock("https://api.github.com")
+    .get("/repo/create/response")
+    .reply(200, JSON.stringify(data.repo.create.response));
+
+    // it('Return an example of response body for create repo', async function() {
+    //     let returnValue = await scrap.retrieveResponseBody("create repo response");
+    //     expect(returnValue).to.equal("");
+    // }); 
+
+    //Test for getting Javascript command for create repo
+    const testOfJavascript = nock("https://api.github.com")
+    .get("/repo/create/javascript")
+    .reply(200, JSON.stringify(data.repo.create.javascript));
+
+    // it('Return an example of javascript command for create repo', async function() {
+    //     let returnValue = await scrap.retrieveJSONExample("create repo javascript");
+    //     expect(returnValue).to.equal("");
+    // }); 
+
+    //Test for getting Javascript command for create repo
+    const testOfAPI = nock("https://api.github.com")
+    .get("/repo/create/path")
+    .reply(200, JSON.stringify(data.repo.create.path));
+
+    // it('Return an example of path command for create repo', async function() {
+    //     let returnValue = await scrap.retrieveAPICall("create repo path");
+    //     expect(returnValue).to.equal("");
+    // });
 
 
 });
