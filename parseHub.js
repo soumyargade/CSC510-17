8b1704b33eaa74be4590faccb58f36cc395bab10
Alexander Chtcheprov <aachtche@ncsu.edu>
2022-03-25 13:48:23 -0400

    Fixed runtime errors. Tidied up validation logic

--- parseHub.js
+++ parseHub.js
@@ -1,21 +1,21 @@
 const axios = require('axios');
-const apiKey = "API_KEY"
+const apiKey = "t0V54k8mSraB"
 
 // TODO: can try to condense this by passing in run_token, api_key as args
 async function getPullsInfo() {
-    const runToken = "YOUR_RUN_TOKEN";
+    const runToken = "tfTyYHqP259h";
     const response = await axios.get(`https://www.parsehub.com/api/v2/runs/${runToken}/data?api_key=${apiKey}&format=json`);
     return response.data;
 }
 
 async function getIssuesInfo() {
-    const runToken = "YOUR_RUN_TOKEN";
+    const runToken = "tsJXRWevaPJr";
     const response = await axios.get(`https://www.parsehub.com/api/v2/runs/${runToken}/data?api_key=${apiKey}&format=json`);
     return response.data;
 }
 
 async function getReposInfo() {
-    const runToken = "YOUR_RUN_TOKEN";
+    const runToken = "tpiT61puGyjq";
     const response = await axios.get(`https://www.parsehub.com/api/v2/runs/${runToken}/data?api_key=${apiKey}&format=json`);
     return response.data;
 }
