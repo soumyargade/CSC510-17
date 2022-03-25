4b64510cb9ed9debf930285594ad0b367f0e0ac8
Alexander Chtcheprov <aachtche@ncsu.edu>
2022-03-25 14:01:40 -0400

    Removing tokens from parseHub.js

--- parseHub.js
+++ parseHub.js
@@ -1,21 +1,21 @@
 const axios = require('axios');
-const apiKey = "t0V54k8mSraB"
+const apiKey = "API_KEY"
 
 // TODO: can try to condense this by passing in run_token, api_key as args
 async function getPullsInfo() {
-    const runToken = "tfTyYHqP259h";
+    const runToken = "YOUR_RUN_TOKEN";
     const response = await axios.get(`https://www.parsehub.com/api/v2/runs/${runToken}/data?api_key=${apiKey}&format=json`);
     return response.data;
 }
 
 async function getIssuesInfo() {
-    const runToken = "tsJXRWevaPJr";
+    const runToken = "YOUR_RUN_TOKEN";
     const response = await axios.get(`https://www.parsehub.com/api/v2/runs/${runToken}/data?api_key=${apiKey}&format=json`);
     return response.data;
 }
 
 async function getReposInfo() {
-    const runToken = "tpiT61puGyjq";
+    const runToken = "YOUR_RUN_TOKEN";
     const response = await axios.get(`https://www.parsehub.com/api/v2/runs/${runToken}/data?api_key=${apiKey}&format=json`);
     return response.data;
 }
